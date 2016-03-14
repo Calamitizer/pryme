51a88e6a034f3ea206cf7a0aaa2bf0c2d20f23ad
Calamitizer <cindersquall@gmail.com>
2016-03-14 05:26:36 -0400

    Some docstrings and lots of multiplicative functions

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # all number theoretic functions, breadth, sigma, etc
-# power generator
-# error: `n` + ' is not a valid input for ' + `f.__name__`
 # decorate Decomposition __add__, __mul__
 # imp. Decomposition.__div__, __sub__
+# make everything take both ints and decorators
+# kwarg to return Decomposition?
+# factor list
+# totative list
+# testing suite (check oeis, n^n, etc)
+# wrappers in class?
+# OEIS class?
+# rename product?
+# move wrappers to submodule
+# make mobius map (fixed point?)
 
 import math
+import numbers
+import fractions
 import operator
 import functools
 import pickle
@@ -26,7 +36,7 @@ class Decomposition(dict):
         dict.__init__(self)
         if type(arg) is int:
             self.n = arg
-            candidates = primes(math.floor(math.sqrt(self.n)))
+            candidates = primes(int(math.floor(math.sqrt(self.n))))
             d = self.n
             for c in candidates:
                 while d % c == 0:
@@ -35,7 +45,7 @@ class Decomposition(dict):
         elif isinstance(arg, dict):
             for d in arg:
                 self[d] = arg[d]
-            self.n = reduce(operator.mul, [pow(d, self[d]) for d in self], 1)
+            self.n = product([pow(d, self[d]) for d in self])
     def __getitem__(self, d):
         assert_prime(d)
         return dict.__getitem__(self, d)
@@ -50,6 +60,8 @@ class Decomposition(dict):
     def __delitem__(self, d):
         self.n /= pow(d, self[d])
         dict.__delitem__(self, d)
+    def __repr__(self):
+        return type(self).__name__ + '(' + str(self.n) + ')'
     def __add__(self, rhs):
         if type(rhs) is int:
             return self + decompose(rhs)
@@ -65,6 +77,24 @@ class Decomposition(dict):
             return Decomposition({d: self[d] + rhs[d] for d in factors})
     def __rmul__(self, lhs):
         return self * lhs
+
+def needs_decomp(f):
+    """
+    Decorator for typing arguments as Decomposition.
+    
+    If the argument not already a Decomposition, this makes one out of it.
+    """
+    @functools.wraps(f)
+    def wrapper(arg):
+        if type(arg) is Decomposition:
+            return f(arg)
+        assert_natural(arg)
+        return f(decompose(arg))
+    return wrapper
+
+#put somewhere, decorate?
+def product(container):
+    return reduce(operator.mul, container, 1)
 
 def memoize(f): # works!
     """
@@ -103,18 +133,24 @@ def memoize_with_log(f): # works!
     print '! Memoized ' + final.__name__
     return final
 
-def optional_arguments(f):
+def optional_arguments(d):
+    """
+    Decorate the input decorator d to accept optional arguments.
+    """
     def wrapped_decorator(*args):
         if len(args) == 1 and callable(args[0]):
-            return f(args[0])
+            return d(args[0])
         else:
             def real_decorator(decoratee):
-                return f(decoratee, *args)
+                return d(decoratee, *args)
             return real_decorator
     return wrapped_decorator
 
 @optional_arguments
 def natural_input(f, m = 1):
+    """
+    Decorate the input function f to only take ints above m.
+    """
     @functools.wraps(f)
     def final(n):
         assert_natural(n, m)
@@ -128,20 +164,39 @@ def with_print_name(f): # works
         return f(*args, **kwds)
     return wrapper
 
+@natural_input
 def primes(n):
+    """
+    Return a list of primes no greater than n.
+    """
     return [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
+@natural_input
 def is_prime(n):
+    """
+    Return whether n is prime.
+    """
     return n in primes(n)
 
 def mt(f):
+    """
+    Return the mobius transform of the input function f.
+    """
+    @natural_input
     def mf(n):
-        return [mu(n/d) * f(d) for d in divisors(n)]
+        return sum([mobius(n/d) * f(d) for d in divisors(n)])
     return mf
-    
-def mu(n):
-    return 0 if any([p >= 2 for _, p in decomp]) else -1 ** (len(decomp) % 2)
-    
+
+@needs_decomp
+def divisors(decomp):
+    print 2
+    """
+    Return a set of all divisors of the argument.
+    """
+    combine = lambda acc, p: set(a * pow(p, e) for a in acc for e in xrange(decomp[p] + 1))
+    return reduce(combine, decomp, {1})
+
+@natural_input
 def decompose(n):
     """
     Return a memoized Decomposition of n.
@@ -172,25 +227,110 @@ def assert_prime(n):
     message = str(n) + ' is not a prime (of type int).'
     assert is_prime(n), message
 
-def needs_decomp(f):
+@natural_input
+def constant(n):
     """
-    Decorator for typing arguments as Decomposition.
+    Return 1.
     
-    If the argument not already a Decomposition, this makes one out of it.
+    Needs to accept a Decomposition.
     """
-    @functools.wraps(f)
-    def wrapper(arg):
-        if type(arg) is Decomposition:
-            return f(arg)
-        assert_natural(arg)
-        return f(decompose(arg))
-    return wrapper
+    return 1
+
+@natural_input
+def identity(n):
+    """
+    Return n.
+    
+    Needs to accept a Decomposition.
+    """
+    return n
+
+@natural_input(0)
+def power_gen(k):
+    """
+    Return a function yielding the k-th power of its argument.
+    """
+    if k == 0:
+        return constant
+    if k == 1:
+        return identity
+    @natural_input
+    def power_k(n):
+        return pow(n, k)
+    return power_k
+
+@natural_input
+def unit(n):
+    return int(n == 1)
+
+@needs_decomp
+def totient(decomp):
+    factors = [1 - fractions.Fraction(1, d) for d in decomp]
+    return int(decomp.n * product(factors))
+
+@needs_decomp
+def mobius(decomp):
+    """
+    Return the mobius function evaluated at the argument.
+    
+    Accepts an int or a Decomposition.
+    Called lowercase-mu in number theory.
+    mobius(n) = (-1)^support(n) if n is square-free, 0 otherwise.
+    """
+    return 0 if any([p >= 2 for _, p in decomp]) else pow(-1, (breadth(decomp) % 2))
+
+@needs_decomp
+def num_divisors(decomp):
+    return product([decomp[d] + 1 for d in decomp])
+
+@needs_decomp
+def sum_divisors(decomp):
+    a = [pow(d, decomp[d] + 1) - 1 for d in decomp]
+    b = [d - 1 for d in decomp]
+    factors = [fractions.Fraction(x, y) for x, y in zip(a, b)]
+    #factors = [fractions.Fraction(pow(d, decomp[d] + 1), pow(d, decomp[d])) for d in decomp]
+    return int(product(factors))
+
+@natural_input(0)
+def sigma_gen(k):
+    if k == 0:
+        return num_divisors
+    if k == 1:
+        return sum_divisors
+    @needs_decomp
+    def sigma_k(decomp):
+        a = [pow(pow(d, decomp[d] + 1), k) - 1 for d in decomp]
+        b = [pow(d, k) - 1 for d in decomp]
+        # rewrite this line, jeez
+        factors = [fractions.Fraction(x, y) for x, y in zip(a, b)]
+        return int(product(factors))
+    return sigma_k
+
+@needs_decomp
+def num_abel(decomp):
+    """
+    Return the number of isodistinct Abelian groups of the given order.
+    """
+    return product([partitions(decomp[p]) for p in decomp])
+
+@natural_input
+def liouville(n):
+    return pow(-1, breadth(n))
+
+@natural_input
+def gamma(n):
+    return pow(-1, breadth(n))
+
+@natural_input
+def ramanujan(n):
+    return 1
 
 @needs_decomp
 def support(decomp):
     """
-    Return the number of distinct prime factors of the argument (int or Decomposition).
+    Return the number of distinct prime factors of the argument.
     
+    Accepts an int or a Decomposition.
     Called lowercase-omega in number theory.
     """
     return len(decomp)
@@ -200,6 +340,7 @@ def breadth(decomp):
     """
     Return the sum of the exponents in the decomposition of the argument.
     
+    Accepts an int or a Decomposition.
     Called uppercase-Omega in number theory.
     """
     return sum([decomp[d] for d in decomp])
---
+++ todo.md
@@ -0,0 +1,15 @@
+## To-Do List
+
+* all number theoretic functions, breadth, sigma, etc
+* decorate Decomposition __add__, __mul__
+* imp. Decomposition.__div__, __sub__
+* make everything take both ints and decorators
+* kwarg to return Decomposition?
+* factor list
+* totative list
+* testing suite (check oeis, n^n, etc)
+* wrappers in class?
+* OEIS class?
+* rename product?
+* move wrappers to submodule
+* make mobius map (fixed point?)
