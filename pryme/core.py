68be5de801184a6d7feb0838cedb818b22649a86
Calamitizer <cindersquall@gmail.com>
2016-03-14 13:32:02 -0400

    Moved decorators to their own file

--- pryme/core.py
+++ pryme/core.py
@@ -2,8 +2,9 @@ import math
 import numbers
 import fractions
 import operator
-import functools
 import pickle
+
+from .decorators import *
 
 class Decomposition(dict):
     """
@@ -64,91 +65,9 @@ class Decomposition(dict):
     def __rmul__(self, lhs):
         return self * lhs
 
-def needs_decomp(f):
-    """
-    Decorator for typing arguments as Decomposition.
-    
-    If the argument not already a Decomposition, this makes one out of it.
-    """
-    @functools.wraps(f)
-    def wrapper(arg):
-        if type(arg) is Decomposition:
-            return f(arg)
-        assert_natural(arg)
-        return f(decompose(arg))
-    return wrapper
-
 #put somewhere, decorate?
 def product(container):
     return reduce(operator.mul, container, 1)
-
-def memoize(f): # works!
-    """
-    Memoization decorator for a function taking a single argument
-    """
-    class Memodict(dict):
-        def __init__(self, f):
-            self.f = f
-        def __call__(self, n):
-            return self[n]
-        def __missing__(self, key):
-            ret = self[key] = f(key)
-            return ret
-    return Memodict(f)
-
-def memoize_with_log(f): # works!
-    """
-    Goal: make the above print
-    """
-    print '! Memoizing ' + f.__name__
-    class Memodict(dict):
-        def __init__(self, f):
-            self.__name__ = f.__name__
-            self.f = f
-        def __call__(self, n):
-            return self[n]
-        def __missing__(self, key):
-            ret = self[key] = self.f(key)
-            return ret
-    f = Memodict(f)
-    @functools.wraps(f)
-    def final(n):
-        log = '! called ' + f.__name__ + '(' + str(n) + ')'
-        print log
-        return f(n)
-    print '! Memoized ' + final.__name__
-    return final
-
-def optional_arguments(d):
-    """
-    Decorate the input decorator d to accept optional arguments.
-    """
-    def wrapped_decorator(*args):
-        if len(args) == 1 and callable(args[0]):
-            return d(args[0])
-        else:
-            def real_decorator(decoratee):
-                return d(decoratee, *args)
-            return real_decorator
-    return wrapped_decorator
-
-@optional_arguments
-def natural_input(f, m = 1):
-    """
-    Decorate the input function f to only take ints above m.
-    """
-    @functools.wraps(f)
-    def final(n):
-        assert_natural(n, m)
-        return f(n)
-    return final
-
-def with_print_name(f): # works
-    @functools.wraps(f)
-    def wrapper(*args, **kwds):
-        print '! ' + f.__name__ + ' called'
-        return f(*args, **kwds)
-    return wrapper
 
 @natural_input
 def primes(n):
---
+++ pryme/decorators.py
@@ -0,0 +1,83 @@
+import functools
+
+def optional_arguments(d):
+    """
+    Decorate the input decorator d to accept optional arguments.
+    """
+    def wrapped_decorator(*args):
+        if len(args) == 1 and callable(args[0]):
+            return d(args[0])
+        else:
+            def real_decorator(decoratee):
+                return d(decoratee, *args)
+            return real_decorator
+    return wrapped_decorator
+
+@optional_arguments
+def natural_input(f, m = 1):
+    """
+    Decorate the input function f to only take ints above m.
+    """
+    @functools.wraps(f)
+    def final(n):
+        assert_natural(n, m)
+        return f(n)
+    return final
+
+def needs_decomp(f):
+    """
+    Decorator for typing arguments as Decomposition.
+    
+    If the argument  not already a Decomposition, this makes one out of it.
+    """
+    @functools.wraps(f)
+    def wrapper(arg):
+        if type(arg) is Decomposition:
+            return f(arg)
+        assert_natural(arg)
+        return f(decompose(arg))
+    return wrapper
+
+def memoize(f): # works!
+    """
+    Memoization decorator for a function taking a single argument
+    """
+    class Memodict(dict):
+        def __init__(self, f):
+            self.f = f
+        def __call__(self, n):
+            return self[n]
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return Memodict(f)
+
+def memoize_with_log(f): # works!
+    """
+    Goal: make the above print
+    """
+    print '! Memoizing ' + f.__name__
+    class Memodict(dict):
+        def __init__(self, f):
+            self.__name__ = f.__name__
+            self.f = f
+        def __call__(self, n):
+            return self[n]
+        def __missing__(self, key):
+            ret = self[key] = self.f(key)
+            return ret
+    f = Memodict(f)
+    @functools.wraps(f)
+    def final(n):
+        log = '! called ' + f.__name__ + '(' + str(n) + ')'
+        print log
+        return f(n)
+    print '! Memoized ' + final.__name__
+    return final
+
+def with_logging(f): # works
+    @functools.wraps(f)
+    def wrapper(*args, **kwds):
+        print '! ' + f.__name__ + ' called'
+        return f(*args, **kwds)
+    return wrapper
