8aa2b4f6abdaef3452d3c1ba41d16b3caedc4c8d
Calamitizer <cindersquall@gmail.com>
2016-03-14 05:27:36 -0400

    Cleanup

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# all number theoretic functions, breadth, sigma, etc
-# decorate Decomposition __add__, __mul__
-# imp. Decomposition.__div__, __sub__
-# make everything take both ints and decorators
-# kwarg to return Decomposition?
-# factor list
-# totative list
-# testing suite (check oeis, n^n, etc)
-# wrappers in class?
-# OEIS class?
-# rename product?
-# move wrappers to submodule
-# make mobius map (fixed point?)
-
 import math
 import numbers
 import fractions
