2717a0105cdc93e96a697b790d6b8169cf6ca812
Calamitizer <cindersquall@gmail.com>
2016-03-23 05:05:40 -0400

    IntPartition

--- pryme/partitions.py
+++ pryme/partitions.py
@@ -61,6 +61,8 @@ class SetPartition(tuple):
         if rep != range(1, self.n + 1):
             message = 'Sub-tuples do not span 1-' + str(self.n)
             raise TypeError(message)
+    def __int__(self):
+        return self.n
     def _flatten(self):
         return sorted([i for part in self for i in part])
     def _checkother(self, other):
@@ -77,24 +79,19 @@ class SetPartition(tuple):
                 return False
         return True
     def __le__(self, other):
-        return self.__eq__(other) or self.__lt__(other)
+        return self == other or self < other
     def __gt__(self, other):
         self._checkother(other)
-        return other.__lt__(self)
+        return other < self
     def __ge__(self, other):
-        return self.__eq__(other) or self.__gt__(other)
+        return self == other or self > other
     def __eq__(self, other):
         self._checkother(other)
         return tuple.__eq__(self, other)
     def __ne__(self, other):
         return not self == other
     def __floordiv__(self, other):
-        return not self.__le__(other) or self.__gt__(other)
-
-class IntPartition(set):
-    def __init__(self, arg):
-        pass
-        # canonical ordering
+        return not self <= other or self > other
 
 def get_setpartitions(n):
     # assert_natural(0)
@@ -119,6 +116,72 @@ def get_setpartitions(n):
         partitions.append(SetPartition(partition))
     return partitions
 
+class IntPartitionPart(int):
+    def __new__(_cls, arg):
+        #assert_natural
+        if type(arg) is not int or arg < 1:
+            message = repr(arg) + ' is not a valid IntPartitionPart'
+            raise TypeError(message)
+        return int.__new__(_cls, arg)
+
+class IntPartition(tuple):
+    def __new__(_cls, arg):
+        parts = []
+        for k in arg:
+            # sometimes not container
+            if type(k) == IntPartitionPart:
+                parts.append(k)
+            else:
+                parts.append(IntPartitionPart(k))
+        return tuple.__new__(_cls, reversed(sorted(parts)))
+    def __init__(self, arg):
+        self.n = sum(self)
+    def __int__(self):
+        return self.n
+    def _checkother(self, other):
+        if type(other) is not IntPartition:
+            raise TypeError
+    def __lt__(self, other):
+        self._checkother(other)
+        if len(self) > len(other):
+            return False
+        for i in len(self):
+            if self[i] > other[i]:
+                return False
+        return True
+    def __le__(self, other):
+        return self == other or self < other
+    def __gt__(self, other):
+        self._checkother(other)
+        return other < self
+    def __ge__(self, other):
+        return self == other or self > other
+    def __eq__(self, other):
+        self._checkother(other)
+        return tuple.__eq__(self, other)
+    def __ne__(self, other):
+        return not self == other
+    def young(self, char='*'):
+        for part in self:
+            print char*part
+    def conj(self):
+        w = list(self)
+        c = []
+        while w != []:
+            c.append(len(w))
+            w = [part - 1 for part in w if part > 1]
+        print c
+        return IntPartition(c)
+    def rank(self):
+        if len(self) == 0:
+            return 0
+        if self[-1] == len(self):
+            return len(self)
+        for k in xrange(2, len(self) + 1):
+            if self[k - 1] < k:
+                break
+        return k - 1
+
 """
 x = SetPartitionPart([1, 2, 4])
 print 'x: ' + `x`
@@ -134,3 +197,11 @@ print P == P1
 """
 for i in xrange(0, 7):
     print len(get_setpartitions(i))
+
+x = IntPartition((2, 6, 2, 1, 1, 1))
+x = IntPartition((4, 3, 3))
+print x
+print len(x)
+x.young()
+y = x.conj()
+print y.rank()
